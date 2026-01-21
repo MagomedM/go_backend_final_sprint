5919a3ed81a0276e9729260bca8b74d6e12859f4
MagomedM <magaha77900@gmail.com>
2026-01-21 23:10:25 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/MagomedM/go_backend_final_sprint
+
+go 1.22.2
+
+replace golf => ./
+
+require golf v0.0.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "golf/pkg/server"
+
+func main() {
+	fmt.Println("Hello World")
+	server.Run()
+}
